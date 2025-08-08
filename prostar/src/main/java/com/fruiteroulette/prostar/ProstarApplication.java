e2d0e33e5a445c58655c182bd99d2ebe1445a695
Om Shrikhande <omshrikhande73@gmail.com>
2025-08-08 15:04:51 +0530

    backend added

---
+++ prostar/src/main/java/com/fruiteroulette/prostar/ProstarApplication.java
@@ -0,0 +1,13 @@
+package com.fruiteroulette.prostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProstarApplication.class, args);
+	}
+
+}
